ab85b4b657f88379f11f4b64eacf0bd7548749a7
saman <sam.karimi727@gmail.com>
2025-06-13 02:51:39 +0330

    update layout.jsx and navbar.jsx update navbar.jsx

--- components/shared/navbar.jsx
+++ components/shared/navbar.jsx
@@ -2,7 +2,7 @@
 
 function Navigationbar() {
   return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" >
