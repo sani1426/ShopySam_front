7372046cdb28cd2a0e17eca442ea529e17b377d6
saman <sam.karimi727@gmail.com>
2025-06-17 01:08:11 +0330

    feat: create page.jsx and page.jsx

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,21 @@
+
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className="min-vh-100 d-flex justify-content-center align-items-center ">
+
+    <div className="w-50 border border-1 border-black p-5 rounded-3 bg-light shadow-lg">
+        <h1 className="text-center mb-5">Login</h1>
+        <form  className="d-flex flex-column gap-4 ">
+
+            <button  className="btn btn-primary mt-3">Submit</button>
+          </form>
+          <h6 className="text-center mt-3">dont have account? <Link href="/register">sign-up</Link></h6>
+    </div>
+</section>
+  )
+}
+
+export default page
